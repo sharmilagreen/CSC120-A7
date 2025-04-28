63dd9d914107dc6d197fa4b0d95d680043670df6
sharmilagreen <sagreen@smith.edu>
2025-04-28 13:48:36 -0400

    fixed house and outlined lib

--- Library.java
+++ Library.java
@@ -1,8 +1,41 @@
 /* This is a stub for the Library class */
-public class Library {
 
+import java.util.Hashtable;
+
+public class Library extends Building implements LibraryRequirements {
+
+    private Hashtable<String, Boolean> collection;
+    
     public Library() {
       System.out.println("You have built a library: 📖");
+    }
+
+    public void addTitle(String title){
+
+    }
+
+    public String removeTitle(String title){
+
+    }
+    
+    public void checkOut(String title){
+
+    }
+
+    public void returnBook(String title){
+
+    }
+
+    public boolean containsTitle(String title){
+
+    }
+
+    public boolean isAvailable(String title){
+
+    }
+
+    public void printCollection(){
+
     }
   
     public static void main(String[] args) {
